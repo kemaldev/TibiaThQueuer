92e577724223a1d7f16cb707ebf896f4424c074b
kemaldev <kemaldev3@gmail.com>
2019-09-28 17:14:19 +0200

    Added models that are going to be used for the database.

---
+++ Models/CharacterList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class CharacterList
+    {
+        public int CharacterListId { get; set; }
+        public ICollection<TibiaCharacter> TibiaCharacters { get; set; }
+        public Account Account { get; set; }
+    }
+}
---
+++ Models/TibiaCharacter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class TibiaCharacter
+    {
+        public int TibiaCharacterId { get; set; }
+        public string Name { get; set; }
+        public string Vocation { get; set; }
+        public int Level { get; set; }
+        public string World { get; set; }
+        public string PVPType { get; set; }
+        public CharacterList CharacterList { get; set; }
+    }
+}
