f6a44a065dcd44a277e51b1242d08272aa6df78e
kemaldev <kemaldev3@gmail.com>
2020-01-21 19:39:01 +0100

    Changed encoding used for the url creation and added unit tests for helper classes.

--- Common/Helpers/UrlHelper.cs
+++ Common/Helpers/UrlHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Web;
 
 namespace Common.Helpers
 {
@@ -8,7 +9,8 @@ namespace Common.Helpers
     {
         public static string FormatTibiaCharacterUrl(string characterName)
         {
-            string formattedName = characterName.Replace(" ", "+");
+            //string formattedName = characterName.Replace(" ", "+");
+            string formattedName = HttpUtility.UrlEncode(characterName, CodePagesEncodingProvider.Instance.GetEncoding(1252));
             string url = $"https://www.tibia.com/community/?subtopic=characters&name={formattedName}";
 
             return url;
---
+++ UnitTests.Common/Helpers/TestUriHelper.cs
@@ -0,0 +1,51 @@
+﻿using Common.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Common.Helpers
+{
+    [TestClass]
+    public class TestUriHelper
+    {
+        [TestMethod]
+        public void FormatTibiaCharacterUrl_CharNameWithoutSpace_ReturnsUrlWithoutPlus()
+        {
+            // Arrange
+            string charName = "Rektrim";
+
+            // Act
+            var url = UrlHelper.FormatTibiaCharacterUrl(charName);
+
+            // Assert
+            Assert.AreEqual("https://www.tibia.com/community/?subtopic=characters&name=Rektrim", url);
+        }
+
+        [TestMethod]
+        public void FormatTibiaCharacterUrl_CharNameWithSpace_ReturnsUrlWithPlus()
+        {
+            // Arrange
+            string charName = "Relentless Rektrim";
+
+            // Act
+            var url = UrlHelper.FormatTibiaCharacterUrl(charName);
+
+            // Assert
+            Assert.AreEqual("https://www.tibia.com/community/?subtopic=characters&name=Relentless+Rektrim", url);
+        }
+
+        [TestMethod]
+        public void FormatTibiaCharacterUrl_CharNameWithForeignLetters_ReturnsEncodedUrl()
+        {
+            // Arrange
+            string charName = "Hälge";
+
+            // Act
+            var url = UrlHelper.FormatTibiaCharacterUrl(charName);
+
+            // Assert
+            Assert.AreEqual("https://www.tibia.com/community/?subtopic=characters&name=H%e4lge", url);
+        }
+    }
+}
